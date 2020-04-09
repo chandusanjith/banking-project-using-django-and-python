f9b32d63d802c4e0a5c2681c69a4512863d96eb9
Chandu sanjith <chandusanjith.t.2610@gmail.com>
2020-04-09 12:08:50 +0000

    admin access

---
+++ mysite/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import mastercustomernumber, masteraccountnumber, cusm, invm, glif
+
+admin.site.register(mastercustomernumber)
+admin.site.register(masteraccountnumber)
+admin.site.register(cusm)
+admin.site.register(invm)
+admin.site.register(glif)
